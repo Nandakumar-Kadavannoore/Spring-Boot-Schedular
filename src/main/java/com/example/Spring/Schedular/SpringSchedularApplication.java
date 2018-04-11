079253ed370f0b79a264d2de03ae2444ae9cefda
nandakumar_kadavannoore <nandakumark6693@gmail.com>
2018-04-11 21:17:30 +0530

    Project SetUp
    Initial Project SetUp

---
+++ src/main/java/com/example/Spring/Schedular/SpringSchedularApplication.java
@@ -0,0 +1,12 @@
+package com.example.Spring.Schedular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSchedularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSchedularApplication.class, args);
+	}
+}
